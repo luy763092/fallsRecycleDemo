0b77fb012135c2f9b8d17b52470acd6069af4e59
卢渊 <yuanlua@isoftstone.com>
2016-06-03 20:42:27 +0800

    [Feature]完成activity书写.完成布局UI搭建

--- app/src/main/java/com/tianfeng/videoplyer/MainActivity.java
+++ app/src/main/java/com/tianfeng/videoplyer/MainActivity.java
@@ -2,12 +2,26 @@ package com.tianfeng.videoplyer;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewDebug;
 
 public class MainActivity extends Activity {
+
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView() {
+        recyclerView = (RecyclerView) findViewById(R.id.id_rv_recyclerview);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
 }
