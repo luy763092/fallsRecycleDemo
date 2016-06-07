77577a067269af62b3023bfa01e779612dea340a
yuanlu <yuanlu>
2016-06-07 21:38:04 +0800

    [Feature]完成自定义控件下拉刷新......

---
+++ app/src/main/java/com/tianfeng/videoplyer/RefreshActivity.java
@@ -0,0 +1,16 @@
+package com.tianfeng.videoplyer;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by LUY on 2016/6/7.
+ */
+public class RefreshActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ativity_refresh);
+    }
+}
